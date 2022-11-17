808575d3ddd6e90c24ad783e434c310745626970
RakibHossainMorul <67518360+RakibHossainMorul@users.noreply.github.com>
2022-11-17 09:17:37 +0600

    food_delivery_app_ui

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:food_delivery_ui/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
+}
---
+++ lib/models/user.dart
@@ -0,0 +1,9 @@
+import 'package:food_delivery_ui/models/order.dart';
+
+class User {
+  final String name;
+  final List<Order> orders;
+  final List<Order> cart;
+
+  User({required this.name, required this.orders, required this.cart});
+}
